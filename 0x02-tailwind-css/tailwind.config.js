832a726adcf3dffcfc17a5e791a4a19a028cccb9
Amadu <amaduhamzajaarah300@gmail.com>
2025-05-14 12:48:50 -0700

    Set up tailwind css

---
+++ 0x02-tailwind-css/tailwind.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  content: [
+    "./src/**/*.{html,js}", // Adjust this path to match your project structure
+    "./public/**/*.html", // Include other directories if necessary
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
